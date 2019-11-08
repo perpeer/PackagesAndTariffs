b9eef1553ebb4948ad194711d0f0c1e2d299ae0a
perpeer <ekremtaskiran@Ekrems-MacBook-Pro.local>
2019-11-08 12:15:40 +0300

    Project initialized

---
+++ PackagesAndTariffs/PackagesAndTariffs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PackagesAndTariffs
+//
+//  Created by Ekrem TAŞKIRAN on 8.11.2019.
+//  Copyright © 2019 Ekrem TAŞKIRAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
